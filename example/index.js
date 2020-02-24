5aaecf88be50775bd4b031f92efb8809696e82a2
skiano <greg.skiano@oneworld.com>
2020-02-24 10:14:43 -0800

    less diff

--- example/index.js
+++ example/index.js
@@ -3,6 +3,7 @@ const { locate, run, report } = require('..');
 
 async function main() {
   const results = await run({
+    raw: true,
     ext: '.test.js',
     root: path.join(__dirname, 'tests'),
     alias: {
@@ -10,7 +11,7 @@ async function main() {
     },
   });
 
-  report(results);
+  console.log(results);
 }
 
 main().catch(e => {
