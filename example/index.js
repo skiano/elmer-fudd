d431ceec06bafa4c3f7c24d39737bd80f91aed11
skiano <greg.skiano@oneworld.com>
2020-02-24 10:16:14 -0800

    single function to run

--- example/index.js
+++ example/index.js
@@ -2,16 +2,13 @@ const path = require('path');
 const { locate, run, report } = require('..');
 
 async function main() {
-  const results = await run({
-    raw: true,
+  await run({
     ext: '.test.js',
     root: path.join(__dirname, 'tests'),
     alias: {
       '@src': path.join(__dirname, 'src'),
     },
   });
-
-  console.log(results);
 }
 
 main().catch(e => {
