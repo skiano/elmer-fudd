d39f24558a5e7696254bbb8ec54d4dc143cb3be2
skiano <greg.skiano@oneworld.com>
2020-02-25 09:08:57 -0800

    adding some mock function capability

--- example/tests/more/functions.test.js
+++ example/tests/more/functions.test.js
@@ -3,6 +3,13 @@ const { test, assert, mockFn } = require('elmer-fudd');
 test({
   name: 'example fake functions',
   spec: () => {
-    assert.ok(true);
+    const mock = mockFn();
+
+    mock.returns('abc');
+
+    const result = mock('123');
+
+    assert.ok(mock.calledWith('123'));
+    assert.equal(result, 'abc');
   }
 })
